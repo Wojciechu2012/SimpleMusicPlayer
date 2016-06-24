0cd0141c659105361020c97b99fba88534c56449
Wojciechu2012 <Wojciechu2012@gmail.com>
2016-06-24 20:15:41 +0200

    Add project files.

---
+++ SimpleMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SimpleMusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LoadFiles = new System.Windows.Forms.Button();
+            this.PlayAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.LoadPlaylistbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadFiles
+            // 
+            this.LoadFiles.Location = new System.Drawing.Point(12, 479);
+            this.LoadFiles.Name = "LoadFiles";
+            this.LoadFiles.Size = new System.Drawing.Size(142, 57);
+            this.LoadFiles.TabIndex = 0;
+            this.LoadFiles.Text = "Add Music";
+            this.LoadFiles.UseVisualStyleBackColor = true;
+            this.LoadFiles.Click += new System.EventHandler(this.LoadFiles_Click);
+            // 
+            // PlayAll
+            // 
+            this.PlayAll.Location = new System.Drawing.Point(173, 479);
+            this.PlayAll.Name = "PlayAll";
+            this.PlayAll.Size = new System.Drawing.Size(142, 57);
+            this.PlayAll.TabIndex = 1;
+            this.PlayAll.Text = "Save Playlist";
+            this.PlayAll.UseVisualStyleBackColor = true;
+            this.PlayAll.Click += new System.EventHandler(this.PlayAll_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 367);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(760, 106);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 500;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Locaction";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(760, 349);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // LoadPlaylistbtn
+            // 
+            this.LoadPlaylistbtn.Location = new System.Drawing.Point(354, 479);
+            this.LoadPlaylistbtn.Name = "LoadPlaylistbtn";
+            this.LoadPlaylistbtn.Size = new System.Drawing.Size(91, 57);
+            this.LoadPlaylistbtn.TabIndex = 5;
+            this.LoadPlaylistbtn.Text = "Wczytaj playliste";
+            this.LoadPlaylistbtn.UseVisualStyleBackColor = true;
+            this.LoadPlaylistbtn.Click += new System.EventHandler(this.LoadPlaylistbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.LoadPlaylistbtn);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.PlayAll);
+            this.Controls.Add(this.LoadFiles);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Media Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadFiles;
+        private System.Windows.Forms.Button PlayAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button LoadPlaylistbtn;
+    }
+}
+
---
+++ SimpleMusicPlayer/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleMusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        string[] files, path;
+        List<string> songlistactivpath = new List<string>();
+        List<string> namesongs = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.FileName = "";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = true;
+        }
+
+        private void LoadFiles_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter = "Mp3Audio(.mp3)|*.mp3";
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try {
+                    files = openFileDialog1.SafeFileNames;
+                    path = openFileDialog1.FileNames;
+               
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        string[] items = new string[2];
+                        items[0] = files[i];
+                        items[1] = path[i];
+                        songlistactivpath.Add(path[i]);
+                        namesongs.Add(path[i]);
+                        ListViewItem lvi = new ListViewItem(items);
+                        listView1.Items.Add(lvi);
+                    }                       
+
+                }catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }
+        }
+
+        private void PlayAll_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Txt.txt|*.txt";
+            
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string[] pathname = new string[listView1.Items.Count];
+                    for (int i = 0; i<songlistactivpath.Count; i++)
+                    {
+                        ListViewItem lv = listView1.Items[i];
+                        pathname[i] =  lv.SubItems[0].Text + "&" + lv.SubItems[1].Text;
+                    }
+                    File.WriteAllLines(saveFileDialog1.FileName, pathname);
+
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }            
+        }
+
+        private void LoadPlaylistbtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Txt(.txt)|*.txt";
+            openFileDialog1.RestoreDirectory = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                path = File.ReadAllLines(openFileDialog1.FileName);
+                songlistactivpath.Clear();
+                listView1.Items.Clear();
+                foreach (string s in path)
+                {
+                    
+                        string[] items = new string[2];
+                        items = s.Split('&');
+                        songlistactivpath.Add(items[1]);
+                        namesongs.Add(items[0]);
+                        ListViewItem lv = new ListViewItem(items);
+                        listView1.Items.Add(lv);
+                    }
+                }catch { MessageBox.Show("To nie jest plik playlisty"); return; }
+                
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1_Enter(axWindowsMediaPlayer1, e);
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count>0)
+                axWindowsMediaPlayer1.URL = songlistactivpath[listView1.FocusedItem.Index];          
+        }         
+
+    }
+}
